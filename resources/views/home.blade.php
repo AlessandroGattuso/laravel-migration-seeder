35f42f835ed1ad00ade1dc2509fc279ef5b369e3
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-21 15:30:53 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <h1 class="text-center mb-5">Trenitalia</h1>
+    <div class="container d-flex flex-wrap gap-5">
+        @foreach($trains as $train)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$train['stazione_di_partenza']}} - {{$train['stazione_di_arrivo']}}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{$train['azienda']}}</h6>
+                    <p class="card-text">{{$train['orario_di_partenza']}} - {{$train['orario_di_arrivo']}}</p>
+                    <p class="card-text text-warning">{{($train['in_orario']) ? "E' in ritardo" : ''}}</p>
+                    <p class="card-text">Codice treno: {{$train['codice_treno']}}</p>
+                    <p class="card-text text-danger">{{($train['in_orario']) ? "Cancellato" : ''}}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</body>
+
+</html>
