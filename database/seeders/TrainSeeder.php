436108117cef6ec9184c4760c9cd4dd731159674
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-22 11:18:25 +0100

    added less records

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -16,14 +16,14 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i=0; $i<=10; $i++){
+        for($i=0; $i<6; $i++){
             $newTrain = new Train();
             $newTrain->azienda = $faker->word();
             $newTrain->stazione_di_partenza = $faker->word();
             $newTrain->stazione_di_arrivo = $faker->word();
-            $newTrain->orario_di_partenza = $faker->time();
-            $newTrain->orario_di_arrivo = $faker->time();
-            $newTrain->codice_treno= $faker->randomNumber(1, false);
+            $newTrain->orario_di_partenza = $faker->time('H:i');
+            $newTrain->orario_di_arrivo = $faker->time('H:i');
+            $newTrain->codice_treno= $faker->randomNumber(5, false);
             $newTrain->numero_carrozze = $faker->numberBetween(20, 50);
             $newTrain->in_orario = $faker->randomElement([true,false]);
             $newTrain->cancellato = $faker->randomElement([true,false]);
